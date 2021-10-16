04815369c0635aaf774f460a668dcd90af709eb8
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-10-16 11:55:52 +0300

    Added other blocks and block library.

--- scripts/BlockDef.js
+++ scripts/BlockDef.js
@@ -1,26 +1,60 @@
-/*Blockly.Blocks['connection'] = {
+var resourceLinkerJson = {
+  "type": "resource_linker",
+  "message0": "%1",
+  "args0": [
+    {
+      "type": "field_variable",
+      "name": "LINKER_VARIABLE",
+      "variable": "Linker ID"
+    }
+  ],
+  "inputsInline": false,
+  "output": "resource_linker",
+  "colour": 150,
+  "tooltip": "A linker value that points to a resource definition.",
+  "helpUrl": ""
+}
+
+Blockly.Blocks['resource_linker'] = {
   init: function() {
-    this.appendValueInput("IP_VALUE_1")
-        .setCheck("String")
-        .appendField(new Blockly.FieldTextInput("IP1"), "IP1");
-    this.appendValueInput("IP_VALUE_2")
-        .setCheck("String")
-        .appendField(new Blockly.FieldTextInput("IP2"), "IP2");
-    this.appendStatementInput("PROCESS")
-        .setCheck(null)
-        .appendField(new Blockly.FieldDropdown([["FTP","FTP"], ["HTTP","HTTP"], ["SMTP","SMTP"]]), "TYPES");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setColour(230);
- this.setTooltip("A block used to initiate a connection between two devices.");
- this.setHelpUrl("");
+    this.jsonInit(resourceLinkerJson);
   }
-};*/
+}
+
+var resourceDefinitionJson = {
+  "type": "resource_definition",
+  "message0": "Resource Definition %1 Resource Linker %2",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "RESOURCE_ID",
+      "check": "resource_linker"
+    }
+  ],
+  "inputsInline": false,
+  "colour": 180,
+  "tooltip": "A definition of a resource.",
+  "helpUrl": ""
+}
+
+Blockly.Blocks['resource_definition'] = {
+  init: function() {
+    this.jsonInit(resourceDefinitionJson);
+  }
+}
 
 var uriRootJson = {
   "type": "uri_root",
-  "message0": "URI Root %1 %2",
+  "message0": "URI Root %1 %2 %3",
   "args0": [
+    {
+      "type": "field_input",
+      "name": "URI_ROOT",
+      "text": "URI Path"
+    },
     {
       "type": "input_dummy"
     },
@@ -47,10 +81,20 @@ Blockly.Blocks['uri_root'] = {
 
 var uriStaticJson = {
   "type": "uri_static",
-  "message0": "Static URI %1 %2",
+  "message0": "Static URI %1 %2 Resource Linker %3 %4",
   "args0": [
     {
+      "type": "field_input",
+      "name": "URI_STATIC",
+      "text": "URI Path"
+    },
+    {
       "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "RESOURCE_LINKER",
+      "check": "resource_linker"
     },
     {
       "type": "input_statement",
@@ -71,7 +115,7 @@ var uriStaticJson = {
     "uri_static",
     "uri_dynamic"
   ],
-  "colour": 30,
+  "colour": 330,
   "tooltip": "A block that defines a static part of the URI scheme.",
   "helpUrl": ""
 }
@@ -79,5 +123,52 @@ var uriStaticJson = {
 Blockly.Blocks['uri_static'] = {
   init: function() {
     this.jsonInit(uriStaticJson);
+  }
+}
+
+var uriDynamicJson = {
+  "type": "uri_dynamic",
+  "message0": "Dynamic URI %1 %2 Resource Linker %3 %4",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "URI_DYNAMIC",
+      "text": "URI Path"
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "RESOURCE_LINKER",
+      "check": "resource_linker"
+    },
+    {
+      "type": "input_statement",
+      "name": "URI",
+      "check": [
+        "uri_static",
+        "uri_dynamic"
+      ]
+    }
+  ],
+  "inputsInline": false,
+  "previousStatement": [
+    "uri_static",
+    "uri_dynamic",
+    "uri_root"
+  ],
+  "nextStatement": [
+    "uri_static",
+    "uri_dynamic"
+  ],
+  "colour": 225,
+  "tooltip": "A block that defines a dynamic part of the URI scheme.",
+  "helpUrl": ""
+}
+
+Blockly.Blocks['uri_dynamic'] = {
+  init: function() {
+    this.jsonInit(uriDynamicJson);
   }
 }
--- scripts/BlockGenerator.js
+++ scripts/BlockGenerator.js
@@ -1,16 +1,20 @@
-Blockly.JavaScript['connection'] = function(block) {
-  var text_ip1 = block.getFieldValue('IP1');
-  var value_ip_value_1 = Blockly.JavaScript.valueToCode(block, 'IP_VALUE_1', Blockly.JavaScript.ORDER_ATOMIC);
-  var text_ip2 = block.getFieldValue('IP2');
-  var value_ip_value_2 = Blockly.JavaScript.valueToCode(block, 'IP_VALUE_2', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_types = block.getFieldValue('TYPES');
-  var statements_process = Blockly.JavaScript.statementToCode(block, 'PROCESS');
+Blockly.JavaScript['resource_linker'] = function(block) {
+  var variable_linker_variable = Blockly.JavaScript.nameDB_.getName(block.getFieldValue('LINKER_VARIABLE'), Blockly.Variables.NAME_TYPE);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['resource_definition'] = function(block) {
+  var value_resource_id = Blockly.JavaScript.valueToCode(block, 'RESOURCE_ID', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   return code;
 };
 
 Blockly.JavaScript['uri_root'] = function(block) {
+  var text_uri_root = block.getFieldValue('URI_ROOT');
   var statements_uri = Blockly.JavaScript.statementToCode(block, 'URI');
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
@@ -18,6 +22,17 @@ Blockly.JavaScript['uri_root'] = function(block) {
 };
 
 Blockly.JavaScript['uri_static'] = function(block) {
+  var text_uri_static = block.getFieldValue('URI_STATIC');
+  var value_resource_linker = Blockly.JavaScript.valueToCode(block, 'RESOURCE_LINKER', Blockly.JavaScript.ORDER_ATOMIC);
+  var statements_uri = Blockly.JavaScript.statementToCode(block, 'URI');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['uri_dynamic'] = function(block) {
+  var text_uri_dynamic = block.getFieldValue('URI_DYNAMIC');
+  var value_resource_linker = Blockly.JavaScript.valueToCode(block, 'RESOURCE_LINKER', Blockly.JavaScript.ORDER_ATOMIC);
   var statements_uri = Blockly.JavaScript.statementToCode(block, 'URI');
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
